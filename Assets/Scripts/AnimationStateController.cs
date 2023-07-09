565abd80d7a9729b43a8ccc32c3f6b8e72b97237
Blueflier <joseph.e.hartono@gmail.com>
2023-07-08 21:03:27 -0700

    added bool hash for performance in animationStateController

--- Assets/Scripts/AnimationStateController.cs
+++ Assets/Scripts/AnimationStateController.cs
@@ -6,25 +6,27 @@ public class AnimationStateController : MonoBehaviour
 {
 
     Animator animator;
+    int isWalkingHash;
 
     // Start is called before the first frame update
     void Start()
     {
         animator = GetComponent<Animator>();
+        isWalkingHash = Animator.StringToHash("isWalking");
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool isWalking = animator.GetBool("isWalking");
+        bool isWalking = animator.GetBool(isWalkingHash);
         bool forwardPressed = Input.GetKey("w");
         if (!isWalking && forwardPressed)
         {
-            animator.SetBool("isWalking", true);
+            animator.SetBool(isWalkingHash, true);
         }
         if (isWalking && !forwardPressed)
         {
-            animator.SetBool("isWalking", false);
+            animator.SetBool(isWalkingHash, false);
         }
     }
 }
